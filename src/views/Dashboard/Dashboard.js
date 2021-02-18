8e8db179ed07b0558ab18683f10c3db14e00bf75
Andres <andrex200316@gmail.com>
2021-02-17 19:44:37 -0500

    Small fixes to SidebarRight

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,20 +2,24 @@ import React from "react";
 import "./Dashboard.scss";
 import Card from "./Card";
 import { GiSofa } from "react-icons/gi";
-
-// function Customcard(name, icon, kw) {}
+import { IoBed, IoLibrary } from "react-icons/io5";
+import { MdKitchen } from "react-icons/md";
+import { FaThermometerHalf } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
 
 function Dashboard() {
   return (
     <React.Fragment>
       <Card title="Living Room" icon={<GiSofa />} value="174" />
-      <Card title="Bedroom" icon={<GiSofa />} value="196" />
-      <Card title="Study Room" icon={<GiSofa />} value="289" />
-      <Card title="Kitchen" icon={<GiSofa />} value="201" />
+      <Card title="Bedroom" icon={<IoBed />} value="196" />
+      <Card title="Study Room" icon={<IoLibrary />} value="289" />
+      <Card title="Kitchen" icon={<MdKitchen />} value="201" />
       <div id="temperature" className="controls">
-        <div className="icon"></div>
+        <div className="icon">
+          <FaThermometerHalf size="2em" color="#6A7E8E" />
+        </div>
         <div className="text">
-          <span>Indoor Temperature</span>
+          <h5>Indoor Temperature</h5>
           <p>
             <span className="up-down-zero">+</span>
             <span className="temp-value">25</span>
@@ -23,7 +27,18 @@ function Dashboard() {
           </p>
         </div>
       </div>
-      <div id="humidity" className="controls"></div>
+      <div id="humidity" className="controls">
+        <div className="icon">
+          <WiHumidity size="2em" color="#6A7E8E" />
+        </div>
+        <div className="text">
+          <h5>Humidity</h5>
+          <p>
+            <span className="humidity-value">25</span>
+            <span className="humidity-units">%</span>
+          </p>
+        </div>
+      </div>
       <div id="light" className="controls"></div>
       <div id="spending">Hello</div>
     </React.Fragment>
